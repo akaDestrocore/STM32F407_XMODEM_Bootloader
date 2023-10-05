bb3665e873dd6f9352f13cfdd2d66724032d98a5
サイバー・デーモン <93255514+akaDestrocore@users.noreply.github.com>
2023-10-05 15:49:12 +0300

    Delete my_libs directory

--- my_libs/xmodem.c
+++
@@ -1,5 +0,0 @@
-#include "xmodem.h"
-
-/* Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-static uint8_t xmodem_packet_number;         		//packet number counter
-static uint32_t xmodem_actual_flash_address; 		//address where we have to write
